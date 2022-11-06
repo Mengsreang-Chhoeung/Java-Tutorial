1ad85c7dd069658ee6cd98cd6a1900aaac2183c2
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-11-06 09:18:24 +0700

    add more exercises to string package

---
+++ src/main/java/com/mengsreang/java/tutorial/string/AppFive.java
@@ -0,0 +1,16 @@
+package com.mengsreang.java.tutorial.string;
+
+public class AppFive {
+    
+    public static void main(String[] args) {
+        
+        // declare variables with escape sequences
+        String txt = "Hello\nWorld";
+        String txtTwo = "Hello\tWorld";
+        // output
+        System.out.print("Text: " + txt);
+        System.out.print("Text Two: " + txtTwo);
+        
+    }
+    
+}
