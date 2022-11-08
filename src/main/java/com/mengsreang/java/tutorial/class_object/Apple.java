bf56482bb0fd7ae605480f48fae488810383e1ff
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-11-08 16:18:54 +0700

    add class_object package

---
+++ src/main/java/com/mengsreang/java/tutorial/class_object/Apple.java
@@ -0,0 +1,6 @@
+package com.mengsreang.java.tutorial.class_object;
+
+// create a class
+public class Apple {
+    String name = "Apple";
+}
---
+++ src/main/java/com/mengsreang/java/tutorial/class_object/Main.java
@@ -0,0 +1,16 @@
+package com.mengsreang.java.tutorial.class_object;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // create multiple objects
+        Apple appleOne = new Apple();
+        Apple appleTwo = new Apple();
+        // output attribute of each objects
+        System.out.println(appleOne.name);
+        System.out.println(appleTwo.name);
+        
+    }
+    
+}
