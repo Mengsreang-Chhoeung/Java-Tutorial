c132682e7e0e25ac9c5181d7ab16d4c6be99df92
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-11-08 17:15:03 +0700

    add modifier package

---
+++ src/main/java/com/mengsreang/java/tutorial/modifier/Fruit.java
@@ -0,0 +1,13 @@
+package com.mengsreang.java.tutorial.modifier;
+
+// create a class
+public class Fruit {
+    // define attributes
+    public String name = "Apple";
+    public Double price = 1.0;
+    
+    // define method
+    public void greeting() {
+        System.out.println("Hello Fruit");
+    }
+}
