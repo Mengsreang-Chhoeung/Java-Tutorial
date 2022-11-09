557b08d144cec100134423134717a3e28f7fb6bb
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-11-09 11:45:40 +0700

    modify value

--- src/main/java/com/mengsreang/java/tutorial/inheritance/single/Main.java
+++ src/main/java/com/mengsreang/java/tutorial/inheritance/single/Main.java
@@ -7,8 +7,8 @@ public class Main {
         // create an object
         Banana banna = new Banana();
         // add values to attribute of object
-        banna.name = "Apple";
-        banna.price = 1.0;
+        banna.name = "Banna";
+        banna.price = 0.5;
         // output
         banna.output();
         
