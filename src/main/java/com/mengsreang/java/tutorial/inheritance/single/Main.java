7f4aeb9766d91901c49ce6f7c2b3c47aa991128d
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-11-09 10:56:35 +0700

    rename object

--- src/main/java/com/mengsreang/java/tutorial/inheritance/single/Main.java
+++ src/main/java/com/mengsreang/java/tutorial/inheritance/single/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
         
         // create an object
-        Banana apple = new Banana();
+        Banana banna = new Banana();
         // add values to attribute of object
-        apple.name = "Apple";
-        apple.price = 1.0;
+        banna.name = "Apple";
+        banna.price = 1.0;
         // output
-        apple.output();
+        banna.output();
         
     }
     
