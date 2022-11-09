86cce84cc11c60c6ab818e037d4a86c3f23953bd
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-11-09 17:00:52 +0700

    add enum package

---
+++ src/main/java/com/mengsreang/java/tutorial/enums/Level.java
@@ -0,0 +1,8 @@
+package com.mengsreang.java.tutorial.enums;
+
+// create an enum class
+public enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/mengsreang/java/tutorial/enums/Main.java
@@ -0,0 +1,11 @@
+package com.mengsreang.java.tutorial.enums;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+        
+    }
+}
